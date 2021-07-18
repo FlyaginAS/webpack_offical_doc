24f56942adf30b15e2441c184653426b313b9123
FlyaginAS <rus.reg.36@gmail.com>
2021-07-18 22:03:47 +0300

    import Data from Json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './1.png';
+import Data from './data.json';
 
 /**starting asset management */
 
 console.log('hello from index.js');
+console.log(Data);
+
 function component() {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
