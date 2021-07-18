23b930a2336eabf6084187b4f4e6277a63822a89
FlyaginAS <rus.reg.36@gmail.com>
2021-07-18 19:17:14 +0300

    modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import _ from 'lodash';
+
 console.log('hello from index.js');
+function component(){
+  const element= document.createElement('div');
+  element.innerHTML=_.join(['Hello','webpack'],' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+/**Note that webpack will not alter any code other than import and export statements */
