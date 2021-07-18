7f32fc97e79fdf9ab7dcf910dde7798fff35f5ed
FlyaginAS <rus.reg.36@gmail.com>
2021-07-18 21:36:06 +0300

    Loading CSS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import _ from 'lodash';
+import './style.css';
 
 /**starting asset management */
 
 console.log('hello from index.js');
-function component(){
-  const element= document.createElement('div');
-  element.innerHTML=_.join(['Hello','webpack'],' ');
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
 
 document.body.appendChild(component());
-/**Note that webpack will not alter any code other than import and export statements */
+/**Note that webpack will not alter any
+ * code other than import and export statements */
